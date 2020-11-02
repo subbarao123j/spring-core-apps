e59979e5bc4e6d94cba5e64c5ac436850ae956a5
kul_Subbarao <kul_Subbarao@KUL-3D3PXZ1.incontrol-tech.local>
2020-11-02 14:32:53 +0800

    app12

---
+++ spring-maven-constructor-CollectionsList2/src/main/java/com/spring/Department.java
@@ -0,0 +1,23 @@
+package com.spring;
+
+import java.util.List;
+
+public class Department {
+	
+	private List<Employee> employees;
+
+/*	public List<Employee> getEmployees() {
+		return employees;
+	}*/
+
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
+
+	public List<Employee> getEmployees() {
+		// TODO Auto-generated method stub
+		return employees;
+	}
+
+
+}
